377a92f7040b7c4bd9d7bbdfbf5b5da03701af39
etro-it <etro_it@etroconstruction.com>
2025-06-17 15:15:07 -0700

    Create get-user-idle-time.ps1

---
+++ get-user-idle-time.ps1
@@ -0,0 +1,30 @@
+# Add .NET type to access user32.dll for idle time
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+
+public static class IdleTimeHelper
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct LASTINPUTINFO
+    {
+        public uint cbSize;
+        public uint dwTime;
+    }
+
+    [DllImport("user32.dll")]
+    public static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+    public static uint GetIdleTime()
+    {
+        LASTINPUTINFO lii = new LASTINPUTINFO();
+        lii.cbSize = (uint)Marshal.SizeOf(lii);
+        GetLastInputInfo(ref lii);
+        return ((uint)Environment.TickCount - lii.dwTime) / 1000;
+    }
+}
+"@
+
+# Call the method and print idle time
+$idleTime = [IdleTimeHelper]::GetIdleTime()
+Write-Output "User idle time: $idleTime seconds"
